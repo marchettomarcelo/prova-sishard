91fb9d3d8338a5bf97f8a6a7c5343d326d179872
Ubuntu <ubuntu@ip-172-31-3-24.us-east-2.compute.internal>
2023-09-27 18:04:52 +0000

    fim

--- ai_sishard_bcc_23_2/q3/solucao.c
+++ ai_sishard_bcc_23_2/q3/solucao.c
@@ -1,15 +1,12 @@
 // Seu código da funcao1_solucao
-long funcao1_solucao(int edi){
 
+long funcao1_solucao(int edi){
     int eax = 2;
     int edx = 1;
 
     while (eax <= edi){                        
         edx *= eax;
         eax++;
-
     }
-    
     return (long) edx;
-
 }
